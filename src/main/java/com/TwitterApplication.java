9d75810376faa0383cd3a040a8309daedfd710f0
No.23 <317848010@qq.com>
2018-05-11 20:16:27 +0800

    --this is for a test dic

--- src/main/java/com/TwitterApplication.java
+++ src/main/java/com/TwitterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwitterApplication {
-
+ //4564564
     public static void main(String[] args) {
         SpringApplication.run(TwitterApplication.class, args);
     }
